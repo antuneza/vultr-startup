e91e51657fdcfc0fbd49be1ea2d1f5fd682f8127
Fernando Antunez <antuneza@gmail.com>
2018-01-13 13:12:24 +0000

    Extra scripts

---
+++ install_php_7.2.sh
@@ -0,0 +1,6 @@
+#~/bin/sh
+sudo apt-get install -y apt-transport-https lsb-release ca-certificates
+sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+sudo sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+sudo apt-get update
+sudo apt-get install -y php7.2-fpm php7.2
