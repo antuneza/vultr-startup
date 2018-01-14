58681111cfa916eea27978a3e3b2be8fa43cf96a
Fernando Antunez <antuneza@gmail.com>
2018-01-14 13:59:28 +0000

    Fix Percona script

--- install_percona_5.7.sh
+++ install_percona_5.7.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 export MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-root}
-#export DEBIAN_FRONTEND=noninteractive
 
 wget https://repo.percona.com/apt/percona-release_0.1-4.$(lsb_release -sc)_all.deb
 sudo dpkg -i percona-release_0.1-4.$(lsb_release -sc)_all.deb
 
-sudo sh -c 'echo "percona-server-server-5.7 percona-server-server-5.7/root-pass password $MYSQL_ROOT_PASSWORD" | debconf-set-selections'
-sudo sh -c 'echo "percona-server-server-5.7 percona-server-server-5.7/re-root-pass password $MYSQL_ROOT_PASSWORD" | debconf-set-selections'
-
 sudo apt-get update
+
+echo "percona-server-server-5.7 percona-server-server-5.7/root-pass password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+echo "percona-server-server-5.7 percona-server-server-5.7/re-root-pass password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+
 sudo apt-get -y install percona-server-server-5.7
 
