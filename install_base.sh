f343d4617e2c1c803ae501146501fb62742ad57a
Fernando Antunez <antuneza@gmail.com>
2018-01-18 18:00:44 +0800

    Server Time

--- install_base.sh
+++ install_base.sh
@@ -5,3 +5,5 @@ sudo apt-get upgrade -y
 
 sudo apt-get install -y build-essential
 
+sudo sh -c 'echo "Asia/Hong_Kong" > /etc/timezone'
+sudo ln -s /usr/share/zoneinfo/Asia/Hong_Kong /etc/localtime 
