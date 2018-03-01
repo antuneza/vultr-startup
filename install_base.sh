ed96db71a745dc64adba8c0ac0604820c45d5487
Fernando Antunez <antuneza@gmail.com>
2018-03-01 17:01:02 +0800

    Git installation

--- install_base.sh
+++ install_base.sh
@@ -6,4 +6,8 @@ sudo apt-get upgrade -y
 sudo apt-get install -y build-essential
 
 sudo sh -c 'echo "Asia/Hong_Kong" > /etc/timezone'
-sudo ln -s /usr/share/zoneinfo/Asia/Hong_Kong /etc/localtime 
+sudo ln -s /usr/share/zoneinfo/Asia/Hong_Kong /etc/localtime
+
+sudo apt-get install -y git
+git config --global user.name $USER_FULL_NAME
+git config --global user.email $USER_EMAIL
